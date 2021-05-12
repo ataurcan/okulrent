fef47f9ee702009b4665b37b92cbb9b03145fc97
ata urcan <31256641+ataurcan@users.noreply.github.com>
2021-05-12 18:39:04 +0300

    v0.3

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA2bOs779wY4U9cWqaMQi4qKaOHilUlswc",
+  authDomain: "rentacar-4c1a7.firebaseapp.com",
+  projectId: "rentacar-4c1a7",
+  storageBucket: "rentacar-4c1a7.appspot.com",
+  messagingSenderId: "15727046251",
+  appId: "1:15727046251:web:671e299bffa7a536ce8b2e",
+};
---
+++ src/usersignup.js
@@ -0,0 +1,29 @@
+const signup = (email, password) => {
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
+};
+const signin = (email, password) => {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
+};
